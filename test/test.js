56eeeaa7bd8c5ece412e0bb1f8969cb227fbc05c
andyaydna <55512086+andyaydna@users.noreply.github.com>
2020-05-02 15:10:57 -0700

    Add files via upload

--- test/test.js
+++ test/test.js
@@ -1,17 +1,35 @@
-var User, app, mongoose, request, server, should, user, agent;
+var chai = require('chai');
+var chaiHttp = require('chai-http');
+var app = require('../app');
+var request  = require("supertest");
+agent = request.agent(app)
+var expect = chai.expect;
 
-should   = require("should");
-mongoose = require("mongoose");
-request  = require("supertest");
-agent = request.agent('http://localhost:1337')
+chai.use(chaiHttp);
 
-  describe('Login test', function () {
-      it('should redirect to /', function (done) {
-        agent
-        .post('/User/LoginUser')
-        .field('username', 'x')
-        .field('password', 'x')
-        .expect('Location','/User/Login?errorMessage=Invalid%20login.')
-        .end(done)
-      })
-})
+describe('App', function() {
+  describe('RegisterUser', function() {
+    it('responds with status 200', function(done) {
+      chai.request(app)
+        .post('/User/RegisterUser')
+        .send({'firstName': 'P', 'lastName': 'T','email':'A', 'username':'s'})
+        .end(function(err, res) {
+          expect(res).to.have.status(200);
+          done();
+        });
+    });
+  });
+});
+
+describe('Login', function() {
+  it('responds with status 200', function(done) {
+    chai.request(app)
+      .post('/auth')
+      .field('username', 'j')
+      .field('password', '1')
+      .end(function(err, res) {
+        expect(res).to.have.status(200);
+        done();
+      });
+  });
+});
