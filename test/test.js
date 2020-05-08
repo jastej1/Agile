2b30c722b8ed39e82bdc88d286f7f45caa2af7ca
jastej1 <55508943+jastej1@users.noreply.github.com>
2020-05-08 12:43:36 -0700

    Add files via upload
    Updated Tests

--- test/test.js
+++ test/test.js
@@ -4,32 +4,116 @@ var app = require('../app');
 var request  = require("supertest");
 agent = request.agent(app)
 var expect = chai.expect;
+let should = chai.should();
+const UserRepo       = require('../Data/UserRepo');
+const _userRepo      = new UserRepo();
+const EventRepo      = require('../Data/EventRepo');
+const _EventRepo     = new EventRepo();
 
 chai.use(chaiHttp);
 
-describe('App', function() {
-  describe('RegisterUser', function() {
-    it('responds with status 200', function(done) {
-      chai.request(app)
+describe('Properly create, login, and delete user', () => {
+  it('it should register properly', (done) => {
+      let user = 
+      {
+        firstName: "james",
+        lastName: "Paul",
+        email: "jpr@gmail.com",
+        username: "james223",
+        password: "123456",
+        passwordConfirm: "123456"
+    }
+    chai.request(app)
         .post('/User/RegisterUser')
-        .send({'firstName': 'P', 'lastName': 'T','email':'A', 'username':'s'})
-        .end(function(err, res) {
-          expect(res).to.have.status(200);
+        .send(user)
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
           done();
-        });
-    });
+        })
+  });
+
+  it('it should login properly', (done) => {
+    let login = 
+    {
+      username: "james223",
+      password: "123456",
+  }
+  chai.request(app)
+      .post('/auth')
+      .send(login)
+      .end((err, res) => {
+            deleter("james223");
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+        done();
+      })
+  });
+  async function deleter(username)
+  {
+    let deletedItem  = await _userRepo.delete(username);
+  }
+
+});
+
+/*Event Testing */
+
+describe('Properly create and delete event', () => {
+  it('it should Create Event properly', (done) => {
+      let event = 
+      {
+        name:           "Study for Node",
+        description:    "Difficult Test",    
+        people:         "Paul",        
+        date:           "2018-06-12T19:30",
+        userID:         "5eb2ee871ee4361d6800dedd"           
+      }
+    chai.request(app)
+        .post('/User/CreationEvent')
+        .send(event)
+        .end((err, res) => {
+              res.should.have.status(200);
+              res.body.should.be.a('object');
+          done();
+        })
+  });
+
+  it('it should delete properly', (done) => {
+    let delete_val = 
+    {
+      _id: "5eb2ee871ee4361d6800dedd",
+    }
+  chai.request(app)
+      .post('/Event/altDelete')
+      .send(delete_val)
+      .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+        done();
+      })
   });
 });
 
-describe('Login', function() {
-  it('responds with status 200', function(done) {
+
+
+
+describe('Create bad event', () => {
+  it('it should NOT Create Event properly', (done) => {
+      let event = 
+      {
+        name:           "",
+        description:    "Difficult Test",    
+        people:         "Paul",        
+        date:           "2018-06-12T19:30",
+        userID:         "5eb2ee871ee4361d6800dedd"           
+      }
     chai.request(app)
-      .post('/auth')
-      .field('username', 'j')
-      .field('password', '1')
-      .end(function(err, res) {
-        expect(res).to.have.status(200);
-        done();
-      });
+        .post('/User/CreationEvent')
+        .send(event)
+        .end((err, res) => {
+              res.should.have.status(404);
+              res.body.should.be.a('object');
+          done();
+        })
   });
 });
