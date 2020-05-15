828552576ac30bd8b27cda8b6fac3971e6112ae1
jastej1 <jdhillon120@my.bcit.ca>
2020-05-14 17:59:44 -0700

    one test added

--- test/test.js
+++ test/test.js
@@ -49,6 +49,26 @@ describe('Properly create, login, and delete user', () => {
         done();
       })
   });
+
+  it('it should update user properly', (done) => {
+    let user = 
+    {
+      firstName: "Bill",
+      lastName: "Smith",
+      email: "jpr@gmail.com",
+      username: "james223",
+      old_username: "james223",
+  }
+  chai.request(app)
+      .post('/user/UpdateUser')
+      .send(user)
+      .end((err, res) => {
+            res.should.have.status(200);
+            res.body.should.be.a('object');
+        done();
+      })
+});
+
   async function deleter(username)
   {
     let deletedItem  = await _userRepo.delete(username);
@@ -117,3 +137,4 @@ describe('Create bad event', () => {
         })
   });
 });
+
